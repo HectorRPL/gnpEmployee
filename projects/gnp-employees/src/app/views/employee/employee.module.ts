bfac6b0fe9885de8d86ae38dc2c7f4a8451bbef6
hectorfv <hector.kaizen@gmail.com>
2021-06-09 22:47:40 -0500

    add angular material

--- projects/gnp-employees/src/app/views/employee/employee.module.ts
+++ projects/gnp-employees/src/app/views/employee/employee.module.ts
@@ -3,6 +3,15 @@ import {CommonModule} from '@angular/common';
 import {EmployeeRoutingModule} from './employee-routing.module';
 import {EmployeeComponent} from './employee/employee.component';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
@@ -12,7 +21,16 @@ import {EmployeeListComponent} from './employee-list/employee-list.component';
   ],
   imports: [
     CommonModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTableModule,
   ]
 })
 export class EmployeeModule {
